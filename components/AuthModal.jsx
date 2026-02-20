c4c7b26521f66709e80a9a9f7e5dd1fcefbaef1f
jeddelich@gmail.com <jeddelich@gmail.com>
2026-02-20 01:30:13 -0500

    add html to AuthModal

--- components/AuthModal.jsx
+++ components/AuthModal.jsx
@@ -1,8 +1,40 @@
-
 function AuthModal() {
   return (
-    <div>AuthModal</div>
-  )
+    <div className="modal">
+      <div className="container">
+        <div className="row">
+          <h2 className="modal__title">Log in to Summarist</h2>
+          <div className="modal__btn--wrapper">
+            <i></i>
+            <button className="modal__btn--login login--guest">
+              Login as a Guest
+            </button>
+          </div>
+          <div className="modal__separator">
+            <span className="modal__separator--text">or</span>
+          </div>
+          <div className="modal__btn--wrapper">
+            <i></i>
+            <button className="modal__btn--login login--google">
+              Login with Google
+            </button>
+          </div>
+          <div className="modal__separator">
+            <span className="modal__separator--text">or</span>
+          </div>
+          <form action="">
+            <input type="email" className="form__input--email" />
+            <input type="password" className="form__input--password" />
+            <button className="modal__btn--login login--main">Login</button>
+          </form>
+          <div className="modal__bottom--questions">
+            <a href="">Forgot your password?</a>
+            <button>Don't have an account? Sign up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AuthModal
+export default AuthModal;
