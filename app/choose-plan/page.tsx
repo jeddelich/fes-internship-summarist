7e309bc2407944291ee6333ebb882412f036af03
jeddelich@gmail.com <jeddelich@gmail.com>
2026-02-27 17:12:35 -0500

    add sticky component

--- app/choose-plan/page.tsx
+++ app/choose-plan/page.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import { IoDocumentTextSharp } from "react-icons/io5";
 import styles from "./page.module.css";
 import { RiPlantFill } from "react-icons/ri";
 import { FaHandshake } from "react-icons/fa";
-import Footer from "@/components/layout/Footer"
+import Footer from "@/components/layout/Footer";
+import Btn from "@/components/ui/Btn";
 
 function page() {
+  
+  function purchase() {
+    console.log("purchase")
+  }
+  
   return (
     <div className={styles.page}>
       <section className={styles.landing}>
@@ -23,68 +31,83 @@ function page() {
         </figure>
       </section>
 
-      <section className={styles.benefits}>
-        <ul className={styles.benefitsList}>
-          <li className={styles.benefit}>
-            <figure className={styles.iconWrapper}>
-              <IoDocumentTextSharp className={styles.icon} />
-            </figure>
-            <div className={styles.benefitDescription}>
-              <span className={styles.bold}>Key ideas in few min</span> with
-              many books to read
-            </div>
-          </li>
-          <li className={styles.benefit}>
-            <figure className={styles.iconWrapper}>
-              <RiPlantFill className={styles.icon} />
-            </figure>
-            <div className={styles.benefitDescription}>
-              <span className={styles.bold}>3 million</span> people growing with
-              Summarist everyday
-            </div>
-          </li>
-          <li className={styles.benefit}>
-            <figure className={styles.iconWrapper}>
-              <FaHandshake className={styles.icon} />
-            </figure>
-            <div className={styles.benefitDescription}>
-              <span className={styles.bold}>Precise recommendations</span>{" "}
-              collections curated by experts
-            </div>
-          </li>
-        </ul>
-      </section>
+      <div className={styles.stickyTrack}>
+        <section className={styles.benefits}>
+          <ul className={styles.benefitsList}>
+            <li className={styles.benefit}>
+              <figure className={styles.iconWrapper}>
+                <IoDocumentTextSharp className={styles.icon} />
+              </figure>
+              <div className={styles.benefitDescription}>
+                <span className={styles.bold}>Key ideas in few min</span> with
+                many books to read
+              </div>
+            </li>
+            <li className={styles.benefit}>
+              <figure className={styles.iconWrapper}>
+                <RiPlantFill className={styles.icon} />
+              </figure>
+              <div className={styles.benefitDescription}>
+                <span className={styles.bold}>3 million</span> people growing
+                with Summarist everyday
+              </div>
+            </li>
+            <li className={styles.benefit}>
+              <figure className={styles.iconWrapper}>
+                <FaHandshake className={styles.icon} />
+              </figure>
+              <div className={styles.benefitDescription}>
+                <span className={styles.bold}>Precise recommendations</span>{" "}
+                collections curated by experts
+              </div>
+            </li>
+          </ul>
+        </section>
 
-      <section className={styles.choosePlan}>
-        <h2 className={styles.h2}>Choose the plan that fits you</h2>
-        <div className={styles.planPackage}>
-          <figure className={styles.selectionCircle}>
-            <div className={styles.selectionDot}></div>
-          </figure>
-          <div className={styles.planDescription}>
-            <h4 className={styles.planTitle}>Premium Plus Yearly</h4>
-            <div className={styles.price}>$99.99/year</div>
-            <div className={styles.trialInfo}>7-day free trial included</div>
+        <section className={styles.choosePlan}>
+          <h2 className={styles.h2}>Choose the plan that fits you</h2>
+          <div className={styles.planPackage}>
+            <figure className={styles.selectionCircle}>
+              <div className={styles.selectionDot}></div>
+            </figure>
+            <div className={styles.planDescription}>
+              <h4 className={styles.planTitle}>Premium Plus Yearly</h4>
+              <div className={styles.price}>$99.99/year</div>
+              <div className={styles.trialInfo}>7-day free trial included</div>
+            </div>
+          </div>
+          <div className={styles.separator}>
+            <hr className={styles.separatorLine} />
+            <div className={styles.separatorText}>or</div>
+            <hr className={styles.separatorLine} />
+          </div>
+          <div className={styles.planPackage}>
+            <figure className={styles.selectionCircle}>
+              <div className={styles.selectionDot}></div>
+            </figure>
+            <div className={styles.planDescription}>
+              <h4 className={styles.planTitle}>Premium Monthly</h4>
+              <div className={styles.price}>$9.99/month</div>
+              <div className={styles.trialInfo}>No trial included</div>
+            </div>
+          </div>
+        </section>
+        <div className={styles.getStarted}>
+          <Btn
+            text="Start your free 7-day trial"
+            color="bg-[#2bd97c]! text-[#032b41]!"
+            style={{ width: "280px" }}
+            wrapper="flex justify-center"
+            onClick={purchase}
+          />
+          <div className={styles.smallPrint}>
+            Cancel your trial at any time before it ends, and you won’t be
+            charged.
           </div>
         </div>
-        <div className={styles.separator}>
-        <hr className={styles.separatorLine}/>
-        <div className={styles.separatorText}>or</div>
-        <hr className={styles.separatorLine}/>
-        </div>
-        <div className={styles.planPackage}>
-          <figure className={styles.selectionCircle}>
-            <div className={styles.selectionDot}></div>
-          </figure>
-          <div className={styles.planDescription}>
-            <h4 className={styles.planTitle}>Premium Monthly</h4>
-            <div className={styles.price}>$9.99/month</div>
-            <div className={styles.trialInfo}>No trial included</div>
-          </div>
-        </div>
-      </section>
+      </div>
 
-    <Footer />
+      <Footer />
     </div>
   );
 }
