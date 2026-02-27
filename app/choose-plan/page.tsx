affc4bdf7087fd75ec4a83492f60b13dbf1bd004
jeddelich@gmail.com <jeddelich@gmail.com>
2026-02-27 13:15:22 -0500

    add non-dynamic choose plan section

--- app/choose-plan/page.tsx
+++ app/choose-plan/page.tsx
@@ -10,9 +10,9 @@ function page() {
         <h1 className={styles.h1}>
           Get unlimited access to many amazing books to read
         </h1>
-        <h2 className={styles.h2}>
+        <h3 className={styles.h3}>
           Turn ordinary moments into amazing learning opportunities
-        </h2>
+        </h3>
         <figure className={styles.imgWrapper}>
           <img
             src="/images/pricing-welcome.svg"
@@ -48,10 +48,39 @@ function page() {
             </figure>
             <div className={styles.benefitDescription}>
               <span className={styles.bold}>Precise recommendations</span>{" "}
-               collections curated by experts
+              collections curated by experts
             </div>
           </li>
         </ul>
+      </section>
+
+      <section className={styles.choosePlan}>
+        <h2 className={styles.h2}>Choose the plan that fits you</h2>
+        <div className={styles.planPackage}>
+          <figure className={styles.selectionCircle}>
+            <div className={styles.selectionDot}></div>
+          </figure>
+          <div className={styles.planDescription}>
+            <h4 className={styles.planTitle}>Premium Plus Yearly</h4>
+            <div className={styles.price}>$99.99/year</div>
+            <div className={styles.trialInfo}>7-day free trial included</div>
+          </div>
+        </div>
+        <div className={styles.separator}>
+        <hr className={styles.separatorLine}/>
+        <div className={styles.separatorText}>or</div>
+        <hr className={styles.separatorLine}/>
+        </div>
+        <div className={styles.planPackage}>
+          <figure className={styles.selectionCircle}>
+            <div className={styles.selectionDot}></div>
+          </figure>
+          <div className={styles.planDescription}>
+            <h4 className={styles.planTitle}>Premium Monthly</h4>
+            <div className={styles.price}>$9.99/month</div>
+            <div className={styles.trialInfo}>No trial included</div>
+          </div>
+        </div>
       </section>
     </div>
   );
