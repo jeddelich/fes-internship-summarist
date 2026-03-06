ba2ffaa250bdda7f0882507c6e625e7eb515270e
jeddelich@gmail.com <jeddelich@gmail.com>
2026-03-05 19:48:39 -0500

    add close button element to search bar

--- components/layout/SearchBar.jsx
+++ components/layout/SearchBar.jsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./SearchBar.module.css";
 import { IoMdSearch } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 function SearchBar() {
+  const [search, setSearch] = useState("");
+  const [showClose, setShowClose] = useState(false);
+
+  function handleSearch(e) {
+    const value = e.target.value
+    
+    setSearch(value);
+    if (value === "" ) {
+      setShowClose(false);
+    } else if (value !== "" && !showClose) {
+      setShowClose(true);
+    }
+  }
+
+  function handleClose() {
+    setSearch("")
+    setShowClose(false)
+  }
+
   return (
     <div className={styles.searchBar}>
       <form action="">
@@ -10,10 +33,16 @@ function SearchBar() {
             type="text"
             placeholder="Search for books"
             className={styles.input}
+            value={search}
+            onChange={handleSearch}
           />
           <div className={styles.button}>
             <figure className={styles.iconWrapper}>
+              {!showClose ? (
                 <IoMdSearch className={styles.icon} />
+              ) : (
+                <IoClose className={`${styles.icon} + " " + ${styles.clickable}`} style={{cursor: "pointer"}} onClick={handleClose} />
+              )}
             </figure>
           </div>
         </div>
