f9c776c27c7b316c8be4ae5098c59f43ae732282
jeddelich@gmail.com <jeddelich@gmail.com>
2026-03-06 22:19:46 -0500

    create custom progress bar on the right with dynamic times

--- components/ui/AudioPlayer.jsx
+++ components/ui/AudioPlayer.jsx
@@ -8,6 +8,10 @@ import { RiForward10Fill, RiReplay10Fill } from "react-icons/ri";
 function AudioPlayer({ img, title, author, audio }) {
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const progress = duration ? (duration - currentTime) * 100 : 0;
 
   function toggleButton() {
     if (!audioRef.current) return;
@@ -22,15 +26,25 @@ function AudioPlayer({ img, title, author, audio }) {
   }
 
   function backwards() {
-    const audio = audioRef.current
-    if (!audio) return
+    const audio = audioRef.current;
+    if (!audio) return;
     audio.currentTime -= 10;
   }
 
   function forwards() {
-    const audio = audioRef.current
-    if (!audio) return
+    const audio = audioRef.current;
+    if (!audio) return;
     audio.currentTime += 10;
+  }
+
+
+  function formatTime(time) {
+    if (!time) return "0:00"
+    
+    const minutes = Math.floor(time / 60)
+    let seconds = Math.floor(time % 60).toString().padStart(2, "0")
+
+    return `${minutes}:${seconds}`
   }
 
   return (
@@ -44,23 +58,43 @@ function AudioPlayer({ img, title, author, audio }) {
           <div className={styles.author}>{author}</div>
         </div>
       </div>
+      <div className={styles.controls}>
+        <button className={styles.buttonSmall} onClick={backwards}>
+          <RiReplay10Fill className={styles.icon} />
+        </button>
+        <button className={styles.buttonBig} onClick={toggleButton}>
+          {isPlaying ? (
+            <FaCirclePause className={styles.icon} />
+          ) : (
+            <FaCirclePlay className={styles.icon} />
+          )}
+        </button>
+        <button className={styles.buttonSmall} onClick={forwards}>
+          <RiForward10Fill className={styles.icon} />
+        </button>
+      </div>
       <div className={styles.audioWrapper}>
-        <div className={styles.controls}>
-          <button className={styles.buttonSmall} onClick={backwards}>
-            <RiReplay10Fill className={styles.icon} />
-          </button>
-          <button className={styles.buttonBig} onClick={toggleButton}>
-            {isPlaying ? (
-              <FaCirclePause className={styles.icon} />
-            ) : (
-              <FaCirclePlay className={styles.icon} />
-            )}
-          </button>
-          <button className={styles.buttonSmall} onClick={forwards}>
-            <RiForward10Fill className={styles.icon} />
-          </button>
-        </div>
-        <audio className={styles.audioBar} src={audio} ref={audioRef}></audio>
+        <audio
+          className={styles.audioBar}
+          src={audio}
+          ref={audioRef}
+          onTimeUpdate={() => setCurrentTime(audioRef.current.currentTime)}
+          onLoadedMetadata={() => setDuration(audioRef.current.duration)}
+        ></audio>
+        <div className={styles.time}>{formatTime(currentTime)}</div>
+        <input
+          className={styles.progress}
+          type="range"
+          min="0"
+          max={duration}
+          value={currentTime}
+          onChange={(e) => {
+              const audio = audioRef.current;
+              if (!audio) return;
+              audio.currentTime = e.target.value;
+            }}
+        />
+        <div className={styles.time}>{formatTime(duration)}</div>
       </div>
     </div>
   );
